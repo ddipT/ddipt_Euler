348c56aec0b2eef09edbc9e4efebc765bec848fc
Tom <48026486+ddipT@users.noreply.github.com>
2020-09-17 22:40:42 +0100

    Solved Euler Problem 2

---
+++ fib_code.py
@@ -0,0 +1,28 @@
+#Each bnew term in the fib sequence is generated bty adding the previous two terms. By starting with 1 and 2, the first 10 will be .... 
+#Consider each and sum whose values do not exceed four million and are even
+
+def sum_even_fib():
+    fib = [1,2]
+    fibeven = []
+    flag = True
+    i=0
+    while flag:
+        if (fib[i-1] + fib[i-2]) < 4000000:
+            fib.append(fib[i-1] + fib[i-2])
+        else:
+            flag = False
+        
+
+    max = len(fib)
+
+    for x in range(max):
+        if fib[x] % 2 == 0:
+            fibeven.append(fib[x])
+
+
+    return sum(fibeven)
+
+print(sum_even_fib())
+
+
+    
