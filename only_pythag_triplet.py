1f505d61da4a9ebd0d2c8159f7be99a36f6fa6c9
Tom <48026486+ddipT@users.noreply.github.com>
2020-09-24 18:49:12 +0100

    added code and file for problem 9

---
+++ only_pythag_triplet.py
@@ -0,0 +1,21 @@
+"""
+A Pythagorean triplet is a set of three natural numbers, a < b < c, for which,
+
+a2 + b2 = c2
+For example, 32 + 42 = 9 + 16 = 25 = 52.
+
+There exists exactly one Pythagorean triplet for which a + b + c = 1000.
+Find the product abc.
+"""
+
+
+
+squares = []
+x=1
+
+while x**2 < 1000:
+    squares.append(x**2)
+    x+=1
+
+# 36 64 and 900
+
