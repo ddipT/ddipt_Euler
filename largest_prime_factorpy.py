85ce80a1494f9844a23418b37cc6b03136af98fb
Tom <48026486+ddipT@users.noreply.github.com>
2020-10-02 19:19:57 +0100

    added comments for prime problem

--- largest_prime_factorpy.py
+++ largest_prime_factorpy.py
@@ -1,6 +1,6 @@
 #The prime factors of 13195 are 5, 7, 13 and 29.
 #What is the largest prime factor of the number 600851475143?
-
+#This is v doable, just need to use the modulus function
 
 num = 600851475143 
 num2 = num
